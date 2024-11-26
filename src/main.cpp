559aaac84b6cf7e98da78cce1b64a8ae1b4769e0
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-26 08:22:47 -0500

    foobarbaz

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
-#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/UILayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
-	bool init() {
-		if (!GJBaseGameLayer::init()) return false;
+class $modify(MyUILayer, UILayer) {
+	// bool init(GJBaseGameLayer*)
+	bool init(GJBaseGameLayer* p0) {
+		if (!UILayer::init(p0)) return false;
 		if (!Mod::get()->getSettingValue<bool>("enabled")) return true;
 		const auto pl = PlayLayer::get();
 		const auto lel = LevelEditorLayer::get();
 		if (!pl && !lel) return true;
-		if (!Mod::get()->getSettingValue<bool>("playLayer") && pl) return true;
-		if (!Mod::get()->getSettingValue<bool>("editorLayer") && lel) return true;
-		auto uiLayer = getChildByID("UILayer");
-		if (!uiLayer) return true;
-		auto fmodMusic = uiLayer->getChildByType<FMODLevelVisualizer>(0);
-		auto fmodSFX = uiLayer->getChildByType<FMODLevelVisualizer>(1);
+		log::info("foo");
+		if (!Mod::get()->getSettingValue<bool>("playLayer") && pl == p0) return true;
+		if (!Mod::get()->getSettingValue<bool>("editorLayer") && lel == p0) return true;
+		log::info("bar");
+		auto fmodMusic = this->getChildByType<FMODLevelVisualizer>(0);
+		auto fmodSFX = this->getChildByType<FMODLevelVisualizer>(1);
 		if (!fmodMusic || !fmodSFX) return true;
-		MyGJBaseGameLayer::repositionMusicAndSFX(fmodMusic, fmodSFX);
+		log::info("baz");
+		MyUILayer::repositionMusicAndSFX(fmodMusic, fmodSFX);
 		return true;
 	}
 	void repositionMusicAndSFX(FMODLevelVisualizer* fmodMusic, FMODLevelVisualizer* fmodSFX) {
